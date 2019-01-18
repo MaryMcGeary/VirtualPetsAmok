d487484b4c81b5d6b0c428173f02c858b4bd36af
alexanderjalbright <alexanderjalbright@gmail.com>
2019-01-18 12:02:41 -0500

    Quit Start in progress

--- VirtualPetsAmok/Program.cs
+++ VirtualPetsAmok/Program.cs
@@ -6,7 +6,23 @@ namespace VirtualPetsAmok
     {
         static void Main(string[] args)
         {
+            //Quit/Start
+            Console.WriteLine("Welcome to Virtual Pets Amok!");
+            Console.WriteLine("Press 0 to Quit");
+
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (menuChoice.Equals(0))
+            {
+                Console.WriteLine("See you next time, friend.");
+                return;
+            }
             
+            
+
+
+            
+
         }
     }
 }
