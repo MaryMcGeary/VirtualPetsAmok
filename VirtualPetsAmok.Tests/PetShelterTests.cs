096af7df1a049922ba69ed2aeb69dce4e19fa808
Alex Albright <alexanderjalbright@gmail.com>
2019-01-29 10:01:02 -0500

    Added not empty test

--- VirtualPetsAmok.Tests/PetShelterTests.cs
+++ VirtualPetsAmok.Tests/PetShelterTests.cs
@@ -10,18 +10,25 @@ namespace VirtualPetsAmok.Tests
         [Fact]
         public void Can_Pet_List_Be_Made()
         {
-            // Arrange
             PetShelter pets = new PetShelter();
-
         }
+
         [Fact]
         public void Is_The_List_Empty()
         {
-            // Arrange
             PetShelter pets = new PetShelter();
 
-            // Assert
             Assert.Empty(pets.OrganicPetsList);
+        }
+
+        [Fact]
+        public void Is_The_List_Not_Empty()
+        {
+            PetShelter pets = new PetShelter();
+
+            pets.OrganicPetsList.Add(new OrganicPet());
+
+            Assert.NotEmpty(pets.OrganicPetsList);
         }
     }
 }
